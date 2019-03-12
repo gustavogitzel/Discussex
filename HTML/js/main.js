e35facd389a7c7edaf86dbabf42d3a7f230c1a68
Gustavo Gitzel <42344174+gustavoFG28@users.noreply.github.com>
2019-03-12 12:00:37 -0300

    Menu Vertical

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -1,11 +1,36 @@
 $(document).ready(function()
 {
+    var menuClose = true;
     $(".menu-icon").click(function(){
         $(".menu-icon").toggleClass("change");
-        $("#menu nav").toggleClass("active");
+
+        if(menuClose)
+            $("#menu nav").animate({
+                left:"0"
+            },300, function(){
+                var fundo = $("div");
+                fundo.id = "fundoPretoMenu";
+                $("body").after(fundo).css({
+                    "width": "100%",
+                    "height": "100%",
+                    "position": "absolute",
+                    "top": "0",
+                    "margin-top":"2em",
+                    "transition":"none",
+                    "background": "rgba(26,26,26)"
+                });
+            });
+        else
+        $("#menu nav").animate({
+            left: "-30em"
+        },300, function(){
+            $("#fundoPretoMenu").remove();
+        });
+
+        menuClose = !menuClose;
     })
     $("#menu").mouseover(function(){
-        $("#logo").attr("src","img/logoDark.png");
+        // $("#logo").attr("src","img/logoDark.png");
     })
     $("#menu").mouseout(function(){
         $("#logo").attr("src","img/logo.png");
