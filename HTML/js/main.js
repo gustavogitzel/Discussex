bede58e1b7625ca380b272c60347e79b5001efb1
Gustavo Gitzel <42344174+gustavoFG28@users.noreply.github.com>
2019-03-15 13:48:20 -0300

    Created menu black

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -1,26 +1,32 @@
 $(document).ready(function()
 {
     var menuClose = true;
+    
+        $("#corpo").click(()=>{
+            if(!menuClose)
+                $(".menu-icon").click();
+        });
+    
     $(".menu-icon").click(function(){
         $(".menu-icon").toggleClass("change");
-
+        alterarMenu(menuClose);
+        menuClose = !menuClose;
+        var div; 
         
         if(menuClose)
         {
-            $("#menu nav").animate({
-                left:"0"
-            },300, function(){});
-            var fundoPreto = "<div id='fundoMenuPreto'></div>";
-            document.getElementById("pagina").innerHTML += fundoPreto;
+            $("#pagina").css("overflow-y", "initial");
+            document.getElementById("corpo").removeChild(div);
         }
-        else
-        $("#menu nav").animate({
-            left: "-30em"
-        },300, function(){
-            $("#fundoPretoMenu").remove();
-        });
-
-        menuClose = !menuClose;
+        if(menuClose == false)
+        {
+            div = document.createElement("div");
+            div.id = "fundoMenuPreto";
+            $("#pagina").css("overflow-y", "hidden");
+            
+            document.getElementById("corpo").appendChild(div);
+        }
+            
     })
     $("#menu").mouseover(function(){
         $("#logo").attr("src","img/logoDark.png");
@@ -29,3 +35,11 @@ $(document).ready(function()
         $("#logo").attr("src","img/logo.png");
     })
 })
+
+function alterarMenu(estaFechado)
+{
+    if(estaFechado)   
+        $("#menu nav").animate({left:"0"},300);
+    else
+        $("#menu nav").animate({left: "-30em"},300);
+}
