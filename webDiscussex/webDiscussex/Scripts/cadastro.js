44d09464b6a0cd2d776486c6cff6592e4da7504a
Gustavo Gitzel <42344174+gustavoFG28@users.noreply.github.com>
2019-04-22 11:58:41 -0300

    aaa

---
+++ webDiscussex/webDiscussex/Scripts/cadastro.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(() => {
+    $("#txtLoginEmail").on("blur",() => {
+        checarEmail();
+    });
+});
+
+function checarEmail() {
+    var url = $("#txtLoginEmail").data('request-url');
+    $.post(url, {
+        emailDigitado: $("#txtLoginEmail").val()
+    },
+        function (data) {
+            if (data == 0) {
+                $("#lbCadastroEmail").addClass("disponivel");
+            }
+            else
+                alert("email indisponivel");
+        });
+}
