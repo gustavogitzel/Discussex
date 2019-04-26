71e9d61f7757d09e1a64e7922d7a77b99b6c5af6
Gustavo Gitzel <42344174+gustavoFG28@users.noreply.github.com>
2019-04-25 22:34:10 -0300

    design mitos e vdds

--- webDiscussex/webDiscussex/Scripts/mitos.js
+++ webDiscussex/webDiscussex/Scripts/mitos.js
@@ -1,10 +1,17 @@
 ﻿$(document).ready(() => {
     $('.verdade').click(function () {
-        var $elementoPai = $(this).parent();
+        var $elementoPai = $(this).parent();        //div
 
         var $elementoAvo = $elementoPai.parent();
 
         var $elemento = $elementoAvo.find('[name = "aparecer"]');
+        
+        if ($elemento.find("h1").html() == "Mentira")
+            $elementoAvo.css("background", "red");
+        else
+            $elementoAvo.css("background", "green");
+
+        $(this).attr("disabled", "true").addClass("desligado").siblings("button").attr("disabled", "true").addClass("desligado");
 
         $elemento.slideDown();
     });
@@ -16,6 +23,15 @@
 
         var $elemento = $elementoAvo.find('[name = "aparecer"]');
 
-        $elemento.slideDown();
+        if ($elemento.find("h1").html() == "Mentira")
+            $elementoAvo.css("background", "red");
+        else 
+            $elementoAvo.css("background", "green");
+
+
+        $(this).attr("disabled", "true").addClass("desligado").siblings("button").attr("disabled", "true").addClass("desligado");
+
+
+        $elemento.slideDown("slow");
     });
 });
