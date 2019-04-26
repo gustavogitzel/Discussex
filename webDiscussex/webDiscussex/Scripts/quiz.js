8f43c9cd79de65886a7fb32d6c619998450f4854
isapsz <42344186+isapsz@users.noreply.github.com>
2019-04-26 13:50:16 -0300

    aaa

--- webDiscussex/webDiscussex/Scripts/quiz.js
+++ webDiscussex/webDiscussex/Scripts/quiz.js
@@ -14,11 +14,14 @@ function exibirPontuacao() {
 
     var pontos = 0;
 
-    for (var i = 0; i < array.length; i++)
-        if ($("[name = #" + array[i] + "_" + i+ "]").checked)
+    for (var i = 0; i < array.length -1; i++) {
+        var certo = $("#sel" + array[i].substring(5, 6) + "_" + i).is(':checked');
+        if (certo)
             pontos++;
+    }
 
-    var txt = "<div id='fundoPreto'></div><div id='areaPontuacao' class='row container pontuacao'><div class='col col-2' id='imgPontos'>"+
+    var txt =
+        "<div id='fundoPreto'></div><div id='areaPontuacao' class='row container pontuacao'><div class='col col-2' id='imgPontos'>" +
         "<img src='/img/guaxinimFeliz.png'></div> <div class='col col-2' > <button class='btn concluir' name= 'btnPontuacao' id= 'btnPontuacao' " +
         "value='Pontuação'>OK</button></div>";
 
