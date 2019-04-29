0671a8adf1303807cbef1624b6fca8f26bf2c50a
isapsz <42344186+isapsz@users.noreply.github.com>
2019-04-29 12:05:59 -0300

    aaa

---
+++ webDiscussex/webDiscussex/Models/Forum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webDiscussex.Models
+{
+    public class Forum
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string PalavraChave { get; set; }
+        public int CodUsuario { get; set; }
+    }
+}
---
+++ webDiscussex/webDiscussex/Models/Resposta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webDiscussex.Models
+{
+    public class Resposta
+    {
+        public int Id { get; set; }
+        public int CodPergunta { get; set; }
+        public int CodUsuario { get; set; }
+
+        public string Resposta { get; set; }
+    }
+}
