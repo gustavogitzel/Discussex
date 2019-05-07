2efcd36b6ea78ef46cbb32e2db11b6009f14d5c4
Gustavo Gitzel <42344174+gustavoFG28@users.noreply.github.com>
2019-05-07 11:28:36 -0300

    as

---
+++ webDiscussex/webDiscussex/Scripts/fundoForum.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(() => {
+    var qtd = $("#corpo").attr("data-qtdLinhas");
+    if (qtd % 2 != 0)
+        $("body").css("background", "#2196f3");
+    else
+        $("body").css("background", "#64b5f6");
+});
