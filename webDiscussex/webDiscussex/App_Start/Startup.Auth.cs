14c89d8d0ba5adace0cd674d304bec9c4e25d782
Gustavo Gitzel <42344174+gustavoFG28@users.noreply.github.com>
2019-05-10 15:36:16 -0300

    sasaa

--- webDiscussex/webDiscussex/App_Start/Startup.Auth.cs
+++ webDiscussex/webDiscussex/App_Start/Startup.Auth.cs
@@ -2,7 +2,9 @@
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.Owin;
+using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.Cookies;
 using Owin;
 
 [assembly: OwinStartup(typeof(webDiscussex.App_Start.Startup))]
@@ -14,6 +16,8 @@ namespace webDiscussex.App_Start
         public void Configuration(IAppBuilder app)
         {
             // Para obter mais informações sobre como configurar seu aplicativo, visite https://go.microsoft.com/fwlink/?LinkID=316888
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            
 
             app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             {
