b6fa268f1f67de85affb4d345c2c1f75ff7b646a
GUSTAVO FERREIRA GITZEL <u18194@academico.cotuca.unicamp.br>
2019-05-17 12:18:45 -0300

    sasa

---
+++ webDiscussex/webDiscussex/Controllers/MapsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Net;
+using System.IO;
+using System.Net.Http;
+
+namespace webDiscussex.Controllers
+{
+    [RoutePrefix("api/maps")]
+    [EnableCors(origins: "http://localhost:61322", headers: "*", methods: "*")]
+    public class MapsController : ApiController
+    {
+        // GET: Maps
+        //public String Get()
+        //{
+        //    WebRequest request = WebRequest.Create(
+        //    "https://maps.googleapis.com/maps/api/place/textsearch/xml?query==posto+de+saude+near+Jardim Campos Eliseos&rankyby=distance&radius=10000&key=AIzaSyBBh6JK23HFsrPff9iyGpdfzztePcfRhq4");
+        //    request.Credentials = CredentialCache.DefaultCredentials;
+
+        //    WebResponse response = request.GetResponse();
+
+        //    using (Stream dataStream = response.GetResponseStream())
+        //    {
+        //        StreamReader reader = new StreamReader(dataStream);
+        //        string responseFromServer = reader.ReadToEnd();
+        //        response.Close();
+        //        return (responseFromServer);
+        //    }
+        //}
+
+        [Route("")]
+
+        public HttpResponseMessage Get()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("GET: Test message")
+            };
+
+        }
+    }
+}
--- webDiscussex/webDiscussex/Global.asax.cs
+++ webDiscussex/webDiscussex/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.Security;
@@ -16,6 +17,16 @@ namespace webDiscussex
             // Código que é executado na inicialização do aplicativo
            AreaRegistration.RegisterAllAreas();
            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            GlobalConfiguration.Configure((config) =>
+            {
+                config.MapHttpAttributeRoutes();
+
+                config.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "api/{controller}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+                );
+            });
         }
     }
 }
